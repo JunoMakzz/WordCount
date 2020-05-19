240cebd87477c6a50d107554d1e451159126614d
unknown <1816138877@qq.com>
2020-05-19 23:31:17 +0800

    first edition

---
+++ WordCount.c
@@ -0,0 +1,68 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int OperatorJudge(char para[3])//判断使用字符计数或者单词计数函数
+{
+	if (para[0] == '-' && para[1] == 'c')
+		return 1;
+	else if (para[0] == '-' && para[1] == 'w')
+		return 2;
+	else
+		return 0;
+}
+
+void CountChar(char filetext[100])//字符计数函数
+{
+	int count = 0;
+	FILE* fp = fopen(filetext, "r");//打开txt文件
+	if (fp == NULL)
+	{
+		printf("文件打开失败或者文件不存在!\n");
+		exit(0);
+	}
+	while (getc(fp) != EOF)
+	{
+		count++;
+	}
+	printf("字符数: %d", count);
+	printf("\n");
+	fclose(fp);//关闭txt文本文件
+}
+
+void CountWord(char filetext[100])
+{
+	int count = 0;
+	char c;
+	FILE* fp = fopen(filetext, "r");
+	if (fp == NULL)
+	{
+		printf("文件打开失败或者文件不存在!\n");
+		exit(0);
+	}
+	while (!feof(fp))
+	{
+		c = fgetc(fp);
+		if (c == ' ' || c == ',')//空格和逗号分隔开的视为单词
+			count++;
+	}
+	printf("单词数: %d", count + 1);
+	printf("\n");
+	fclose(fp);
+}
+
+int main()
+{
+	char para[3], filetext[100];
+	int result = 0;
+	printf("请输入控制参数('-c'或'-w')和文本文件名，中间用空格隔开\n");
+	scanf("%s", para);
+	scanf("%s", filetext);
+	result = OperatorJudge(para);
+	if (result == 0)
+		printf("控制参数输入错误!");
+	else if (result == 1)
+		CountChar(filetext);
+	else if (result == 2)
+		CountWord(filetext);
+}
